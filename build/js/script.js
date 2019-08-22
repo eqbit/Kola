811c159903ced8903f5b97ec9ce47d6b7276a417
eqbit <eqbits@gmail.com>
2019-08-22 07:29:42 +0500

    initial

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,45 @@
 "use strict";
 
-var test = function test(d) {
-  return 'test';
-};
+$(function () {
+  var state = {
+    delta: 0
+  };
+  var $pageSlider = $('[data-main-slider]');
+  $pageSlider.on('init', function () {
+    mouseWheel($pageSlider);
+  }).slick({
+    vertical: true,
+    dots: true,
+    appendDots: '.pages-dots',
+    infinite: false,
+    arrows: false,
+    verticalSwiping: true
+  });
+  $('[data-next-page]').on('click', function () {
+    $pageSlider.slick('slickNext');
+  });
+
+  function mouseWheel($slider) {
+    $(window).on('wheel', {
+      $slider: $slider
+    }, mouseWheelHandler);
+  }
+
+  function mouseWheelHandler(event) {
+    var $slider = event.data.$slider;
+    state.delta += event.originalEvent.deltaY;
+
+    if (state.delta > 100) {
+      $slider.slick('slickNext');
+      state.delta = 0;
+    }
+
+    if (state.delta < -100) {
+      $slider.slick('slickPrev');
+      state.delta = 0;
+    }
+
+    console.log(state.delta);
+  }
+});
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,42 @@
-$(function() {
-    $('[data-main-slider]').slick({
-        vertical: true,
-        dots: true,
-        appendDots: '.pages-dots',
-        infinite: false,
-        arrows: false,
-        verticalSwiping: true,
+$(function () {
+  let state = {
+    delta: 0
+  }
+  
+  const $pageSlider = $('[data-main-slider]');
+  $pageSlider
+    .on('init', () => {
+      mouseWheel($pageSlider)
     })
+    .slick({
+    vertical: true,
+    dots: true,
+    appendDots: '.pages-dots',
+    infinite: false,
+    arrows: false,
+    verticalSwiping: true
+  });
+  
+  $('[data-next-page]').on('click', function() {
+    $pageSlider.slick('slickNext');
+  });
+  
+  function mouseWheel($slider) {
+    $(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+  }
+  function mouseWheelHandler(event) {
+    const $slider = event.data.$slider;
+    state.delta += event.originalEvent.deltaY;
+    
+    if(state.delta > 100) {
+      $slider.slick('slickNext');
+      state.delta = 0;
+    }
+    if(state.delta < -100) {
+      $slider.slick('slickPrev')
+      state.delta = 0;
+    }
+    
+    console.log(state.delta)
+  }
 });
